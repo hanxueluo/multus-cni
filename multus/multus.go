9202a1123b774bfe4caad6d49796e25f5f703ecd
Huanle Han <hanhuanle@caicloud.io>
2019-03-28 11:35:09 +0800

    fix: fix incorrect netconf parse for ConfList (#4)

--- multus/multus.go
+++ multus/multus.go
@@ -481,6 +481,13 @@ func cmdDel(args *skel.CmdArgs, exec invoke.Exec, kubeClient k8s.KubeClient) err
 		}
 		// First delegate is always the master plugin
 		in.Delegates[0].MasterPlugin = true
+
+		for _, conf := range in.Delegates {
+			if conf.Conf.Type == "" && conf.ConfList.Plugins != nil &&
+				len(conf.ConfList.Plugins) > 0 && conf.ConfList.Plugins[0].Type != "" {
+				conf.ConfListPlugin = true
+			}
+		}
 	}
 
 	// unset the network status annotation in apiserver, only in case Multus as kubeconfig
